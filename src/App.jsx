6123bb10a15df56b8b0f0cc2ebae1fec01641094
KarandeepKaur266 <69587532+KarandeepKaur266@users.noreply.github.com>
2025-10-17 15:04:19 +0530

    Add files via upload
    made changes in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AdminDashboard from './components/AdminDashboard'
-import EmployeeDashboard from './components/EmployeeDashboard'
+import AdminDashboard from "AdminDashboard";
+import EmployeeDashboard from "EmployeeDashboard";
+import Header from "../layout/Header"
+import Footer from "../layout/Footer"
+import SidebarNav from "../layout/SidebarNav"
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+    <Header />  
+      <SidebarNav />
+      <Routes>
+        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/" element={<EmployeeDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
